bc1cb00d13fd07fec6354e1922a7455e2b82e2c4
liwuchen <liwuchen@florens.com>
2023-01-12 16:42:22 +0800

    ✨ feat: update test_cvxpy

--- test_cvxpy.py
+++ test_cvxpy.py
@@ -1,7 +1,7 @@
 import numpy as np
 import time
 import cvxpy as cp
-n = 20000
+n = 10
 
 a = np.random.randint(1, 10, size=n)
 b = np.random.randint(1, 10, size=n)
@@ -19,17 +19,16 @@ objective = cp.Maximize(cp.sum(cp.multiply(x,a)))
 # constraints
 constraints = []
 
-# constraints.append(\
-#     cp.sum_largest(cp.hstack([\
-#         cp.sum(cp.multiply(x, b)), \
-#         cp.sum(cp.multiply(x, c)), \
-#         cp.sum(cp.multiply(x, d))]), 3) <= n*2)
-constraints.append(cp.sum(cp.multiply(x,a)) >= n/2)
-constraints.append(cp.sum(cp.multiply(x,a)) <= n)
-constraints.append(cp.sum(cp.multiply(x,b)) <= n/2)
-constraints.append(cp.sum(cp.multiply(x,c)) >= n/3)
-constraints.append(cp.sum(cp.multiply(x,d)) >= n/4)
-constraints.append(cp.sum(cp.multiply(x,e)) <= n/3)
+constraints.append(cp.sum_largest(cp.hstack([
+    cp.sum(cp.multiply(x, b)), 
+    cp.sum(cp.multiply(x, c)), 
+    cp.sum(cp.multiply(x, d))]), 1) <= 10)
+# constraints.append(cp.sum(cp.multiply(x,a)) >= n/2)
+# constraints.append(cp.sum(cp.multiply(x,a)) <= n)
+# constraints.append(cp.sum(cp.multiply(x,b)) <= n/2)
+# constraints.append(cp.sum(cp.multiply(x,c)) >= n/3)
+# constraints.append(cp.sum(cp.multiply(x,d)) >= n/4)
+# constraints.append(cp.sum(cp.multiply(x,e)) <= n/3)
 # constraints.append(cp.sum(cp.multiply(x,a)) >= n)
 # constraints.append(cp.sum(cp.multiply(x,a)) >= n)
 # constraints.append(cp.sum(cp.multiply(x,a)) >= n)
@@ -45,7 +44,6 @@ For constraints like `x*a > 100 if x*a != 0`, we can treat x*a as semi-continuou
 
 
 prob = cp.Problem(objective, constraints)
-
 # solve model
 prob.solve(solver=cp.CBC, verbose=True, maximumSeconds=100, numberThreads=4)
 print("status:", prob.status)
